5ade327ca0eda78a9ac5af70495f9fc3b0248ab8
tortuepin <kohei-suzuki@cybozu.co.jp>
2021-07-17 15:15:57 +0900

    add service

---
+++ pkg/service/log.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/tortuepin/tolog_ddd/pkg/domain/model"
+	"github.com/tortuepin/tolog_ddd/pkg/domain/repository"
+)
+
+type LogServiceInterface interface {
+	NewLog([]model.Tag, model.LogContent) error
+	EditLog(model.Log, model.Log) error
+	ReadLogs() ([]model.Log, error)
+}
+
+type LogService struct {
+	reader  repository.Reader
+	creater repository.Creater
+	updater repository.Updater
+}
+
+func NewLogService(reader repository.Reader, creater repository.Creater, updater repository.Updater) (LogService, error) {
+	return LogService{reader, creater, updater}, nil
+}
+
+func (s *LogService) NewLog(tags []model.Tag, content model.LogContent) error {
+	t, err := model.NewLogTimeNow()
+	if err != nil {
+		return fmt.Errorf("failed in NewLogTimeNow(): %w", err)
+	}
+
+	log, err := model.NewLog(t, tags, content)
+	if err != nil {
+		return fmt.Errorf("failed in NewLog(): %w", err)
+	}
+
+	if err := s.creater.Create(log); err != nil {
+		return fmt.Errorf("failed in Write(): %w", err)
+	}
+
+	return nil
+}
+
+func (s *LogService) EditLog(from model.Log, to model.Log) error {
+	err := s.updater.Update(from, to)
+	if err != nil {
+		return fmt.Errorf("failed in Updater(): %w", err)
+	}
+	return nil
+}
+
+func (s *LogService) ReadLogs() ([]model.Log, error) {
+	return s.reader.Read()
+}
---
+++ pkg/service/log_test.go
@@ -0,0 +1,162 @@
+package service_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/tortuepin/tolog_ddd/pkg/domain/model"
+	"github.com/tortuepin/tolog_ddd/pkg/domain/repository"
+	"github.com/tortuepin/tolog_ddd/pkg/service"
+)
+
+type fakeReader struct {
+	repository.Reader
+	fakeRead func() ([]model.Log, error)
+}
+
+func (f *fakeReader) Read() ([]model.Log, error) {
+	return f.fakeRead()
+}
+
+type fakeCreater struct {
+	repository.Creater
+	fakeCreate func(model.Log) error
+}
+
+func (f *fakeCreater) Create(log model.Log) error {
+	return f.fakeCreate(log)
+}
+
+type fakeUpdater struct {
+	repository.Updater
+	fakeUpdater func(model.Log, model.Log) error
+}
+
+func (f *fakeUpdater) Update(from model.Log, to model.Log) error {
+	return f.fakeUpdater(from, to)
+}
+
+func Test_NewLog(t *testing.T) {
+	type args struct {
+		tags    []model.Tag
+		content model.LogContent
+	}
+	type fields struct {
+		creater repository.Creater
+	}
+	tests := []struct {
+		name   string
+		args   args
+		fields fields
+		want   error
+	}{
+		{
+			name: "",
+			args: args{
+				tags:    []model.Tag{},
+				content: model.LogContent{},
+			},
+			fields: fields{
+				creater: &fakeCreater{
+					fakeCreate: func(model.Log) error {
+						return nil
+					},
+				},
+			},
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := service.NewLogService(nil, tt.fields.creater, nil)
+			got := s.NewLog(tt.args.tags, tt.args.content)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("service.NewLog() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func Test_EditLog(t *testing.T) {
+	type args struct {
+		from model.Log
+		to   model.Log
+	}
+	type fields struct {
+		updater repository.Updater
+	}
+	tests := []struct {
+		name   string
+		args   args
+		fields fields
+		want   error
+	}{
+		{
+			name: "",
+			args: args{
+				model.Log{},
+				model.Log{},
+			},
+			fields: fields{
+				updater: &fakeUpdater{
+					fakeUpdater: func(model.Log, model.Log) error {
+						return nil
+					},
+				},
+			},
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := service.NewLogService(nil, nil, tt.fields.updater)
+			got := s.EditLog(tt.args.from, tt.args.to)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("service.EditLog() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func Test_ReadLogs(t *testing.T) {
+	type fields struct {
+		reader repository.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []model.Log
+		wantErr bool
+	}{
+		{
+			name: "",
+			fields: fields{
+				reader: &fakeReader{
+					fakeRead: func() ([]model.Log, error) {
+						return []model.Log{}, nil
+					},
+				},
+			},
+			want: []model.Log{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s, _ := service.NewLogService(tt.fields.reader, nil, nil)
+			got, err := s.ReadLogs()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("model.NewLogContent() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("service.EditLog() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
