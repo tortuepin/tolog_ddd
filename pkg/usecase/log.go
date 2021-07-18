6c68b0043bc099a5790cc01889eaee02a383488a
tortuepin <kohei-suzuki@cybozu.co.jp>
2021-07-18 09:59:14 +0900

    Change usecase.LogNew's arg to LogServiceInterface from LogService

--- pkg/usecase/log.go
+++ pkg/usecase/log.go
@@ -4,7 +4,7 @@ import "fmt"
 import "github.com/tortuepin/tolog_ddd/pkg/service"
 import "github.com/tortuepin/tolog_ddd/pkg/domain/model"
 
-func LogNew(service service.LogService, tagstrings []string) error {
+func LogNew(service service.LogServiceInterface, tagstrings []string) error {
 	tags, err := model.NewTags(tagstrings)
 	if err != nil {
 		return fmt.Errorf("error in usecase.LogNew(): %w", err)
